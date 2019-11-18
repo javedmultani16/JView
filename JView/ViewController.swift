509808673d97638e7d08d20ba6eb917899351a07
Javid Multani <javid.multani@contentcreation.com.my>
2019-11-18 12:30:55 +0800

    Initial Commit

---
+++ JView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JView
+//
+//  Created by Javed Multani on 18/11/2019.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
